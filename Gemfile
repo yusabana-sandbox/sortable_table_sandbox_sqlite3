c0a4f09508b130523dac48a4c42f816f76960f8a
Yuji Takaesu <taka.esu.user@gmail.com>
2014-08-24 16:02:22 +0900

    add ranked_model gems and manage row_order

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'spring',        group: :development
 gem 'bootstrap-sass'
 gem 'high_voltage'
 gem 'slim-rails'
+gem 'ranked-model'
+
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_21]
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140824064241) do
+ActiveRecord::Schema.define(version: 20140824065819) do
 
   create_table "fruits", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "row_order"
   end
 
 end
